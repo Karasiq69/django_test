f826685abb360bb5980617663bc40b06db867257
Karasiq69 <128066037+Karasiq69@users.noreply.github.com>
2024-03-04 14:22:31 +0300

    Added allowed hosts = all

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v7+4qt_pvt7u8&3rkh&5+@ue3v^*d-6p4#lh2p_$%i!k0u*o18
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
